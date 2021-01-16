e4d7fee7720c63e9bf656c63271dd25fded0c4d9
Brian Greco <blgreco72dev@gmail.com>
2021-01-16 11:03:58 -0500

    Added Initial Demo Project
    This will be used for each of the specific tutorial examples.

---
+++ Examples/Source/Demo/src/Demo.WebApi/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetFusion.Bootstrap.Container;
+using NetFusion.Builder;
+using NetFusion.Serilog;
+using Serilog;
+using Serilog.Events;
+using Demo.WebApi.Plugin;
+
+namespace Demo.WebApi
+{
+    // Initializes the application's configuration and logging then delegates 
+    // to the Startup class to initialize HTTP pipeline related settings.
+    public class Program
+    {
+        // Allows changing the minimum log level of the service at runtime.
+        private static readonly LogLevelControl LogLevelControl = new LogLevelControl();
+        
+        public static async Task Main(string[] args)
+        {
+            IHost webHost = BuildWebHost(args);
+            
+            var compositeApp = webHost.Services.GetService<ICompositeApp>();
+            var lifetime = webHost.Services.GetService<IHostApplicationLifetime>();
+
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                compositeApp.Stop();
+                Log.CloseAndFlush();
+            });
+                  
+            await compositeApp.StartAsync();
+            await webHost.RunAsync();    
+        }
+
+        private static IHost BuildWebHost(string[] args) 
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
+                .ConfigureLogging(SetupLogging)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureServices(sc =>
+                    {
+                        // Register Log Level Control so it can be injected into
+                        // a service at runtime to change the level.
+                        sc.AddLogLevelControl(LogLevelControl);
+                    });
+                })
+                .UseSerilog()
+                .Build();
+        }
+
+        private static void SetupConfiguration(HostBuilderContext context, 
+            IConfigurationBuilder builder)
+        {
+            builder.AddAppSettings(context.HostingEnvironment);
+        }
+
+        private static void SetupLogging(HostBuilderContext context, 
+            ILoggingBuilder builder)
+        {
+            var seqUrl = context.Configuration.GetValue<string>("logging:seqUrl");
+
+            // Send any Serilog configuration issue logs to console.
+            Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+            
+            var logConfig = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(LogLevelControl.Switch)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+
+                .Enrich.FromLogContext()
+                .Enrich.WithCorrelationId()
+                .Enrich.WithHostIdentity(WebApiPlugin.HostId, WebApiPlugin.HostName)
+                
+                .WriteTo.ColoredConsole();
+
+            if (! string.IsNullOrEmpty(seqUrl))
+            {
+                logConfig.WriteTo.Seq(seqUrl);
+            }
+            
+            Log.Logger = logConfig.CreateLogger();
+        }
+    }
+}
---
+++ Examples/Source/Demo/src/Demo.WebApi/Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetFusion.Builder;
+using NetFusion.Messaging.Logging;
+using NetFusion.Messaging.Plugin;
+using NetFusion.Rest.Server.Plugin;
+using NetFusion.Serilog;
+using NetFusion.Settings.Plugin;
+using NetFusion.Web.Mvc.Plugin;
+using Serilog;
+using Demo.App.Plugin;
+using Demo.Domain.Plugin;
+using Demo.Infra.Plugin;
+using Demo.WebApi.Hubs;
+using Demo.WebApi.Plugin;
+
+namespace Demo.WebApi
+{
+    // Configures the HTTP request pipeline and bootstraps the NetFusion application container.
+    public class Startup
+    {
+        // Microsoft Abstractions:
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _hostingEnv;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnv)
+        {
+            _configuration = configuration;
+            _hostingEnv = hostingEnv;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.CompositeContainer(_configuration, new SerilogExtendedLogger())
+                .AddSettings()
+                .AddMessaging()
+                .AddWebMvc()
+                .AddRest()
+ 
+                .AddPlugin<InfraPlugin>()
+                .AddPlugin<AppPlugin>()
+                .AddPlugin<DomainPlugin>()
+                .AddPlugin<WebApiPlugin>()
+                .Compose();
+
+            services.AddCors();
+            services.AddHttpContextAccessor();
+            services.AddControllers();
+
+            // Adds SignalR and a message log sink that delegates
+            // all received messages to a SignalR Hub.
+            if (_hostingEnv.IsDevelopment())
+            {
+                services.AddSignalR();
+                services.AddMessageLogSink<HubMessageLogSink>();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            string viewerUrl = _configuration.GetValue<string>("Netfusion:ViewerUrl");
+            if (! string.IsNullOrWhiteSpace(viewerUrl))
+            {
+                app.UseCors(builder => builder.WithOrigins(viewerUrl)
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .WithExposedHeaders("WWW-Authenticate","resource-404")
+                    .AllowAnyHeader());
+            }
+            
+            app.UseSerilogRequestLogging();
+            
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                // Register the SignalR Hub that a development tool can connect
+                // for receiving logs of published and received messages.
+                if (env.IsDevelopment())
+                {
+                    endpoints.MapHub<MessageLogHub>("/api/message/log");
+                }
+            });
+        }
+    }
+}
