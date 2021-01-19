7868fb5fddd1b134be6025802e266d348cb397d2
Brian Greco <blgreco72dev@gmail.com>
2021-01-18 20:50:44 -0500

    Updated all Example Nugets to Current Version on Nuget

---
+++ Examples/Source/build-examples.sh
@@ -0,0 +1,21 @@
+dotnet clean ./CQRS/src
+dotnet clean ./Demo/src
+dotnet clean ./DemoSubscriber/src/Demo.Subscriber
+dotnet clean ./Mappings/src
+dotnet clean ./MongoDb/src
+dotnet clean ./RabbitMq/src
+dotnet clean ./RabbitMqSubscriber/src/Demo.Subscriber
+dotnet clean ./ServiceBus/src
+dotnet clean ./ServiceBusSubscriber/src/Demo.Subscriber
+dotnet clean ./Settings/src
+
+dotnet build ./CQRS/src
+dotnet build ./Demo/src
+dotnet build ./DemoSubscriber/src/Demo.Subscriber
+dotnet build ./Mappings/src
+dotnet build ./MongoDb/src
+dotnet build ./RabbitMq/src
+dotnet build ./RabbitMqSubscriber/src/Demo.Subscriber
+dotnet build ./ServiceBus/src
+dotnet build ./ServiceBusSubscriber/src/Demo.Subscriber
+dotnet build ./Settings/src
