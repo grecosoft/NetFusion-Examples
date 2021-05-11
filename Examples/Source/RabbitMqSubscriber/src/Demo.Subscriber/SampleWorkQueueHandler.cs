d0c0673c09da370d215fda080986483fc1a75f7d
Brian Greco <blgreco72dev@gmail.com>
2021-05-11 07:28:55 -0400

    Updated Examples

--- Examples/Source/RabbitMqSubscriber/src/Demo.Subscriber/SampleWorkQueueHandler.cs
+++ Examples/Source/RabbitMqSubscriber/src/Demo.Subscriber/SampleWorkQueueHandler.cs
@@ -2,12 +2,20 @@ using System;
 using Demo.Subscriber.Commands;
 using NetFusion.Common.Extensions;
 using NetFusion.Messaging;
+using NetFusion.RabbitMQ.Publisher;
 using NetFusion.RabbitMQ.Subscriber;
 
 namespace Demo.Subscriber
 {
     public class SampleWorkQueueHandler : IMessageConsumer
     {
+        private readonly IQueueResponseService _queueResponse;
+        
+        public SampleWorkQueueHandler(IQueueResponseService queueResponse)
+        {
+            _queueResponse = queueResponse;
+        }
+        
         [WorkQueue("testBus", "GenerateAndSendEmail")]
         public void GenerateEmail(SendEmail email)
         {
