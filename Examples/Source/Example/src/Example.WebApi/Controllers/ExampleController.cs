b63abf465b4fdfb581a66803d2ac81cfd000ec89
Brian Greco <blgreco72dev@gmail.com>
2023-03-09 20:01:55 -0500

    Replaced with .net core template

--- Examples/Source/Example/src/Example.WebApi/Controllers/ExampleController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Examples.RabbitMq.WebApi.Controllers;
-
-[ApiController, Route("api/[controller]")]
-public class ExampleController : ControllerBase
-{
-    
-}
