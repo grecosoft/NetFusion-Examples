571709486f7a9e053442b2fbf00412de85599bfd
Brian Greco <blgreco72dev@gmail.com>
2023-03-11 15:14:38 -0500

    Deleted old examples

--- Examples/Source/_Examples.RabbitMq/src/Components/Examples.RabbitMq.Domain/Entities/TaxCalc.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Examples.RabbitMq.Domain.Entities;
-
-public class TaxCalc
-{
-    public decimal Amount { get; set; }
-    public DateTime DateCalculated { get; set; }
-}
