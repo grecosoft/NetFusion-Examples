e27ddd116dc3cbb2e7f3e6daad93975ee6bcd526
Brian Greco <blgreco72dev@gmail.com>
2023-03-23 19:44:44 -0400

    Deleted old example

--- Examples/Source/HealthCheck/src/Components/Demo.App/Services/IRunningRepositoryMonitor.cs
+++
@@ -1,9 +0,0 @@
-using NetFusion.Bootstrap.Plugins;
-
-namespace Demo.App.Services;
-
-public interface IRunningRepositoryMonitor : IPluginModuleService
-{
-    void AddRunningRepo(string repositoryId);
-    string RemoveRunningRepo();
-}
--- Examples/Source/HealthCheck/src/Components/Demo.App/Services/IRunningServicesMonitor.cs
+++
@@ -1,9 +0,0 @@
-using NetFusion.Bootstrap.Plugins;
-
-namespace Demo.App.Services;
-
-public interface IRunningServicesMonitor : IPluginModuleService
-{
-    void AddRunningService(string serviceId);
-    string RemoveRunningService();
-}
